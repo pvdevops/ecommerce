1ec50e9ebc5f6c62e1e024d79dcf6c6f2f491fda
EC2 Default User <ec2-user@ip-172-31-19-62.ec2.internal>
2017-02-07 17:34:58 +0000

    Made few bug fixes in Feb 2017

--- onceclick.c
+++ onceclick.c
@@ -2,3 +2,5 @@ This module is for OneClick feature.
 Code is implemented for support OnceClick functionality
 
 Fixes in oneclick modules
+
+Adding some more lines of code to fix few issues dated Feb 2017 for next release
